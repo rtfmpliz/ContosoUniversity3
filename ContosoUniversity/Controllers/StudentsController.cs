27870994781298336e91c3abc2fac724d181328f
rtfmpliz <rtfmpliz@gmail.com>
2018-02-15 21:01:16 +0700

    hang di create complex model

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -20,10 +20,50 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Students.ToListAsync());
-        }
+public async Task<IActionResult> Index(
+    string sortOrder,
+    string currentFilter,
+    string searchString,
+    int? page)
+{    
+        ViewData["CurrentSort"] = sortOrder;
+    ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+    ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+        
+    if (searchString != null)
+    {
+        page = 1;
+    }
+    else
+    {
+        searchString = currentFilter;
+    }
+        ViewData["CurrentFilter"] = searchString;
+    var students = from s in _context.Students
+                   select s;
+                       if (!String.IsNullOrEmpty(searchString))
+    {
+        students = students.Where(s => s.LastName.Contains(searchString)
+                               || s.FirstMidName.Contains(searchString));
+    }
+    switch (sortOrder)
+    {
+        case "name_desc":
+            students = students.OrderByDescending(s => s.LastName);
+            break;
+        case "Date":
+            students = students.OrderBy(s => s.EnrollmentDate);
+            break;
+        case "date_desc":
+            students = students.OrderByDescending(s => s.EnrollmentDate);
+            break;
+        default:
+            students = students.OrderBy(s => s.LastName);
+            break;
+    }
+    int pageSize = 3;
+    return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), page ?? 1, pageSize));
+}
 
         // GET: Students/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -240,7 +280,7 @@ public async Task<IActionResult> DeleteConfirmed(int id)
         //Log the error (uncomment ex variable name and write a log.)
         return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
     }
-// }
+ }
 //         [HttpPost, ActionName("Delete")]
 //         [ValidateAntiForgeryToken]
 //         public async Task<IActionResult> DeleteConfirmed(int id)
--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ContosoUniversity.Models
 {
     public enum Grade
     {
@@ -9,6 +11,7 @@
     {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
+                [DisplayFormat(NullDisplayText = "No grade")]
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
 
