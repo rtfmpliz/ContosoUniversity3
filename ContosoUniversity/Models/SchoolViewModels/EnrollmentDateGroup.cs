76a8d4b2b948eb83cdcbbd687edf804186eee403
rtfmpliz <rtfmpliz@gmail.com>
2018-02-17 21:21:07 +0700

    sebelum ganti sqlite

--- ContosoUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
+++ ContosoUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -1,25 +1,13 @@
-using System.Collections.Generic;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ContosoUniversity.Models
+namespace ContosoUniversity.Models.SchoolViewModels
 {
-    public class Course
+    public class EnrollmentDateGroup
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        [Display(Name = "Number")]
-        public int CourseID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
 
-        [StringLength(50, MinimumLength = 3)]
-        public string Title { get; set; }
-
-        [Range(0, 5)]
-        public int Credits { get; set; }
-
-        public int DepartmentID { get; set; }
-
-        public Department Department { get; set; }
-        public ICollection<Enrollment> Enrollments { get; set; }
-        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public int StudentCount { get; set; }
     }
 }
