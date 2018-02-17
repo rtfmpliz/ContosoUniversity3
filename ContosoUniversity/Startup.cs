76a6560811913c84238ca49fa7099d52a40ed223
rtfmpliz <rtfmpliz@gmail.com>
2018-02-17 22:05:23 +0700

    sqlite kurang karna limitation

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -23,7 +23,10 @@ namespace ContosoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SchoolContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<SchoolContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+    services.AddDbContext<SchoolContext>(options =>
+            options.UseSqlite("Data Source=MvcMovie.db"));
             services.AddMvc();
         }
 
